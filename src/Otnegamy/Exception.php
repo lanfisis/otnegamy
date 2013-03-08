3ea32cad57ee254fef962c807cd7d1f72d6d968e
lanfisis <lanfisis@gmail.com>
2013-03-08 14:57:56 +0100

    First method to analyse if given module exists ad if we can parse config file

--- src/Otnegamy/Exception.php
+++ src/Otnegamy/Exception.php
@@ -32,4 +32,23 @@ namespace Otnegamy;
  */
 class Exception extends \Exception
 {
+    /**
+     * TThis function allows you to turn a warning into an exception.
+     * Use for DOMDocument::loadXML
+     * 
+     * @param int    $errno   Level of the error
+     * @param string $errstr  Error message
+     * @param string $errfile Filename that the error was raised
+     * @param int    $errline Line number the error was raised at
+     * 
+     * @return void
+     * @throws self
+     */
+    public function throwConfigException($errno, $errstr, $errfile, $errline)
+    {
+        if ($errno == E_WARNING) {
+            throw new self('This is not a Magento module config file !');
+            //throw new \DOMException($errstr);
+        }  
+    }
 }
--- src/Otnegamy/Parser.php
+++ src/Otnegamy/Parser.php
@@ -34,7 +34,36 @@ use \Otnegamy\Exception;
  */
 class Parser
 {
+    /**
+     * Config as xml document
+     * 
+     * @var DOMDocument 
+     */
+    protected $config;
+    
+    /**
+     * Build a parsr obejct to work with
+     * 
+     * @param string $path Absolute path of the Magento module to parse
+     * 
+     * @return void
+     */
     public function __construct($path) 
+    {
+        if ($this->isMagentoModule($path)) {
+            $this->config = $this->loadConfig(rtrim($path, '/').'/etc/config.xml');
+        }
+    }
+    
+    /**
+     * Check that it is a Magento module ie. there is a config file.
+     * 
+     * @param string $path Absolute path of the module
+     * 
+     * @return boolean
+     * @throws \Otnegamy\Exception
+     */
+    public function isMagentoModule($path)
     {
         if (!is_string($path) or !is_dir($path)) {
             throw new Exception('This module directory not seem to exist !');
@@ -42,5 +71,22 @@ class Parser
         if (!file_exists(rtrim($path, '/').'/etc/config.xml')) {
             throw new Exception('This directory does not seem to be a Magento module !');
         }
+        return true;
+    }
+    
+    /**
+     * Try to load a Mageto module config file.
+     * 
+     * @param path $config Absolute path to a Mageno module config file 
+     * 
+     * @return \DOMDocument
+     */
+    public function loadConfig($config)
+    {
+        set_error_handler(array('\Otnegamy\Exception', 'throwConfigException'));
+        $dom = new \DOMDocument();
+        $dom->load($config);    
+        restore_error_handler();
+        return $dom;
     }
 }
